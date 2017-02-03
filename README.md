88f1492f4edad492f07e66fd2d47890885f983c9
Gil Birdsong <gibbirdsong@gmail.com>
2017-02-02 18:51:58 -0600

    added readme, removed vestigial code, etc.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Appia
+
+This application utilizes the Google Maps API to provide information on
+Ancient Rome. Places on the map may be accessed through either the present
+markers, or through the list set under the title div.
+
+## Installation
+
+All that is required for successful installation is the presence of all
+necessary files.
+
+## Usage
+
+All one must do to use the application is launch 'index.html' with a web
+browser. An internet connection is required.
+
+## Credits
+
+Application was created thanks to:
+
+The Google Maps API
+Knockout.js
+Getty Images
+Wikipedia
--- js/nemi.js
+++ js/nemi.js
@@ -2,44 +2,44 @@ var map;
 var markers = [];
 // Where the places are.
 var initialPlaces = [
-  {name: "Foro Romano",
-   location: {lat: 41.892451, lng: 12.485324},
-   period: "Pre-Imperial",
-   description: "The Roman Forum was the center of the Roman Empire"},
-  {name: "Septimus Severus Arch",
-   location: {lat: 41.892884, lng: 12.484744},
-   period: "Pre-Imperial",
-   description: "This arch was built to honor a Roman victory over the"
-   + "Parthians. Built in the days of the Roman Republic.",},
-  {name: "The Pantheon",
-   location: {lat: 41.898603, lng: 12.476873},
-   period: "Imperial",
-   description: "Built on the Field of Mars, this famous temple was built to"
-   + "honor all of the Roman gods at once."},
-  {name: "The Colosseum",
-   location: {lat: 41.890197, lng: 12.492237},
-   period: "Imperial",
-   description: "The site of many gladitorial games, executions, and various"
-    + " other forms of public entertainment, the Colloseum remains a "
-    + "testament to peoples' love of both blood and showmanship."},
-  {name: "Circus Maximus",
-   location: {lat: 41.886243, lng: 12.485150},
-   period: "Imperial",
-   description: "Latin for 'Largest Circus', the Circus Maximus was the site of"
-    + " various forms of entertainment. It remains one of Rome's best preserved"
-    + " sites, perhaps owing its state to its incredible size."},
-  {name: "Palatine Hill",
-   location: {lat: 41.888605, lng: 12.488407},
-   period: "Pre-Imperial",
-   description: "The centermost hill of the 7 hills of Rome, the Palatine hill"
-    + " was, in antiquity, thought to be home to the cave in which Romulus"
-    + " and Remus were reared by wolves."},
-  {name: "Tiber River",
-   location: {lat: 41.888664, lng: 12.479562},
-   period: "",
-   description: "The main river flowing through Rome, and the end of many"
-    + " sewers. Much commerce was done here; its shoreline was a bustling"
-    + " marketplace."},
+    {name: "Foro Romano",
+     location: {lat: 41.892451, lng: 12.485324},
+     period: "Pre-Imperial",
+     description: "The Roman Forum was the center of the Roman Empire"},
+    {name: "Septimus Severus Arch",
+     location: {lat: 41.892884, lng: 12.484744},
+     period: "Pre-Imperial",
+     description: "This arch was built to honor a Roman victory over the"
+     + "Parthians. Built in the days of the Roman Republic.",},
+    {name: "The Pantheon",
+     location: {lat: 41.898603, lng: 12.476873},
+     period: "Imperial",
+     description: "Built on the Field of Mars, this famous temple was built to"
+     + "honor all of the Roman gods at once."},
+    {name: "The Colosseum",
+     location: {lat: 41.890197, lng: 12.492237},
+     period: "Imperial",
+     description: "The site of many gladitorial games, executions, and various"
+      + " other forms of public entertainment, the Colloseum remains a "
+      + "testament to peoples' love of both blood and showmanship."},
+    {name: "Circus Maximus",
+     location: {lat: 41.886243, lng: 12.485150},
+     period: "Imperial",
+     description: "Latin for 'Largest Circus', the Circus Maximus was the site of"
+      + " various forms of entertainment. It remains one of Rome's best preserved"
+      + " sites, perhaps owing its state to its incredible size."},
+    {name: "Palatine Hill",
+     location: {lat: 41.888605, lng: 12.488407},
+     period: "Pre-Imperial",
+     description: "The centermost hill of the 7 hills of Rome, the Palatine hill"
+      + " was, in antiquity, thought to be home to the cave in which Romulus"
+      + " and Remus were reared by wolves."},
+    {name: "Tiber River",
+     location: {lat: 41.888664, lng: 12.479562},
+     period: "",
+     description: "The main river flowing through Rome, and the end of many"
+      + " sewers. Much commerce was done here; its shoreline was a bustling"
+      + " marketplace."},
     {name: "Trajan's Column",
      location: {lat: 41.895829, lng: 12.484305},
      period: "Imperial",
@@ -59,7 +59,7 @@ var initialPlaces = [
 var periods = ['Pre-Imperial', 'Imperial', 'Post-Imperial'];
 
 var setMap = function() {
-    // Initiation of google map.
+    // Initiation of Google map.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.889827, lng: 12.486559},
         mapTypeId: 'satellite',
@@ -71,14 +71,8 @@ var setMap = function() {
     setPlaces();
 };
 
-var stopAnimation = function(marker) {
-    // Sets timeout for marker animation after one is clicked.
-    setTimeout(function () {
-        marker.setAnimation(null);
-    }, 5000);
-};
-
 var setPlaces = function() {
+
     for (i in historicPlaces()) {
         var place = historicPlaces()[i];
         marker = new google.maps.Marker({
@@ -110,10 +104,17 @@ var setPlaces = function() {
     };
 };
 
+var stopAnimation = function(marker) {
+    // Sets timeout for marker animation after one is clicked.
+    setTimeout(function () {
+        marker.setAnimation(null);
+    }, 5000);
+};
+
 var filterPlaces = function(period) {
+    //Set visibility of markers according to period selected
     for (i = 0; i < markers.length; i++) {
         marker = markers[i];
-      // If is same category or category not picked
         if(marker.period == period || period.length == 0) {
             marker.setVisible(true);
         } else {
@@ -123,6 +124,8 @@ var filterPlaces = function(period) {
 }
 
 var requestWiki = function(object) {
+    //Clear error message if present
+    $('#error-msg').text('');
 
     var wikiTimeout = setTimeout(function(){
       $('#error-msg').text("Unable to find Wikipedia articles for " + object);
@@ -131,22 +134,19 @@ var requestWiki = function(object) {
     var request = $.ajax({
         url: 'https://en.wikipedia.org/w/api.php?action='
             + 'opensearch&search='+ object +'&format=json',
-        dataType: "jsonp",
-        jsonpCallback: "onJSONPLoad"
+        dataType: "jsonp"
       })
       .done(function(response) {
-            clearTimeout(wikiTimeout);
-        //Return the first and (theoretically) most relavent link.
-            var wikiLink = response[3][0]
-            //Set link value to modal's "Learn more" link.
-            $('#wiki-link').attr("href", wikiLink);
-            //Trigger opening of secondary 'wiki' modal upon click.
-            $('#wiki-link').on('click', function() {
-                $('#wiki-modal').modal('show');
-
+          clearTimeout(wikiTimeout);
+              //Return the first and (theoretically) most relavent link
+              var wikiLink = response[3][0]
+              //Set link value to modal's "Learn more" link.
+              $('#wiki-link').attr("href", wikiLink);
+              //Trigger opening of secondary 'wiki' modal upon click.
+              $('#wiki-link').on('click', function() {
+                  $('#wiki-modal').modal('show');
             });
-        })
-
+      })
 };
 
 var requestGetty = function(object) {
@@ -162,9 +162,9 @@ var requestGetty = function(object) {
         }
     })
     .done(function(data){
-        console.log("Success with data");
+        //Add the first 9 images returned from Getty to modal
         for(var i = 0;i< 9;i++) {
-            $("#image-container").prepend("<img class='modal-image' src='"
+            $("#image-container").append("<img class='modal-image' src='"
                 + data.images[i].display_sizes[0].uri + "'/>");
          }
     })
@@ -174,10 +174,10 @@ var requestGetty = function(object) {
 };
 
 var Place = function(data) {
-  this.name = ko.observable(data.name);
-  this.location = ko.observable(data.location);
-  this.period = ko.observable(data.period);
-  this.description = ko.observable(data.description);
+    this.name = ko.observable(data.name);
+    this.location = ko.observable(data.location);
+    this.period = ko.observable(data.period);
+    this.description = ko.observable(data.description);
 };
 
 var ViewModel = function() {
@@ -201,11 +201,11 @@ var ViewModel = function() {
 
     this.filterPlacesList = function() {
         self.historicPlacesList([]);
-        console.log(self.selectedPeriod());
         for (i = 0; i < initialPlaces.length; i++) {
             var place = initialPlaces[i];
             if (initialPlaces[i].period == self.selectedPeriod() ||
                 self.selectedPeriod() == undefined) {
+                //If given place's period matches selected period, add to list
                 self.historicPlacesList.push( new Place(place) );
             }
         }
@@ -223,6 +223,7 @@ var ViewModel = function() {
                 console.log(marker.name);
             }
         }
+        //Execute API call functions to fill modal
         requestWiki(self.selectedPlace().name());
         requestGetty(self.selectedPlace().name());
         map.setZoom(17);
